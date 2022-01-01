b20e7b7403dc6d69cee37189d43ffaa4712ece86
Abid Al Wassie <abidalwassie@outlook.com>
2022-01-01 19:22:53 +0600

    fixed opt toggle bug

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-window.onload = function() {
-  
 
-  const optBtn = document.querySelector("#optBtn");
-  
-  optMenu.addEventListener('click', optToggle);
-  
-  function optToggle() {
-    const optMenu = document.querySelector("#dropdownMenu");
-    optMenu.classList.toggle("hidden");
-  }
+function optToggle() {
+  const optMenu = document.querySelector("#optMenu");
+  optMenu.classList.toggle("hidden");
 }
