9ef49850079f19ece90ea7eaa1449a41c2147b62
Abid Al Wassie <abidalwassie@outlook.com>
2022-01-02 22:17:02 +0600

    a working mobile nav

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ function optToggle() {
   const optMenu = document.querySelector("#optMenu");
   optMenu.classList.toggle("hidden");
 }
+
+function navToggle() {
+  const navMenu = document.querySelector("#navMenu");
+  navMenu.classList.toggle("hidden");
+}
