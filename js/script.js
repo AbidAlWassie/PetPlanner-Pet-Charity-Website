c2bc0ee8ad78fcf9200acb1d818e5c821a3fd316
Abid Al Wassie <abidalwassie@outlook.com>
2022-01-02 23:57:34 +0600

    updated project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+// window.onload = function() {
 
-function optToggle() {
-  const optMenu = document.querySelector("#optMenu");
-  optMenu.classList.toggle("hidden");
-}
+  // window.addEventListener("scroll", function() {
+  //   nav = document.getElementById("navbar");
+  //   nav.classList.toggle("sticky", window.scrollY > 0);
+  // });
 
-function navToggle() {
-  const navMenu = document.querySelector("#navMenu");
-  navMenu.classList.toggle("hidden");
-}
+  
+  function optToggle() {
+    const optMenu = document.querySelector("#optMenu");
+    optMenu.classList.toggle("hidden");
+  }
+  
+  function navToggle() {
+    const navMenu = document.querySelector("#navMenu");
+    navMenu.classList.toggle("hidden");
+  }
+// }
